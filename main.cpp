544cc96e60c3039652a48d7cd53b3a04fed8b33f
maksim mihailov <>
2021-04-27 18:07:18 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int m,k, counter=0;
+
+cout << "ghfgdf m>k";
+cin >> m >>k;
+for (int i=1; i<m;i++)
+{
+  if (i%k==0) {counter++;}
+
+}
+cout << counter;
+return 0;
+}
